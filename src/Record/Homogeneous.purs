9ed19be5c8b390839a6f081695fb7b0780682a00
Serhii Khoma <srghma@gmail.com>
2020-07-10 15:24:02 +0300

    feat: remove map because it is already implemented in justinwoo/purescript-record-extra, remove also other functions except foldlValues, make it work, implement foldlValuesWithKey also

--- src/Record/Homogeneous.purs
+++ src/Record/Homogeneous.purs
@@ -1,278 +1,120 @@
 module Record.Homogeneous
-  ( valuesToUnfoldable
-
-  , mapValues
-  , class MapValues
-  , mapValuesImpl
-
-  , mapWithIndex
-  , class MapWithIndex
-  , mapWithIndexImpl
-
-  , foldlValues
+  ( foldlValues
   , class FoldlValues
   , foldlValuesImpl
 
-  , foldrValues
-  , class FoldrValues
-  , foldrValuesImpl
-
-  , foldrValuesLazy
-  , class FoldrValuesLazy
-  , foldrValuesLazyImpl
+  , foldlValuesWithIndex
+  , class FoldlValuesWithIndex
+  , foldlValuesWithIndexImpl
   ) where
 
-import Prelude
-
-import Control.Lazy as Z
-import Data.Maybe (Maybe(..))
-import Record (get)
-import Record.Builder (Builder, build, insert)
-import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
-import Data.Tuple (Tuple(..))
-import Data.Unfoldable (class Unfoldable, unfoldr)
-import Type.Row (class Lacks)
-import Type.RowList (class RowToList, RLProxy(..))
+import Record (get) as Record
+import Type.Prelude (class IsSymbol, RLProxy(RLProxy), SProxy(SProxy), reflectSymbol)
 import Type.Row.Homogeneous (class Homogeneous, class HomogeneousRowList)
-import Prim.RowList
-
-valuesToUnfoldable
-  :: forall r fields f v
-   . RowToList r fields
-  => FoldrValuesLazy fields r v
-  => Unfoldable f
-  => Record r
-  -> f v
-valuesToUnfoldable r = unfoldr (\(LazyTupleList f) -> f unit) lazyTupleList
-  where
-  lazyTupleList = foldrValuesLazy
-    (\v f -> (LazyTupleList \_ -> Just (Tuple v f)))
-    (LazyTupleList (\_ -> Nothing))
-    r
-
-newtype LazyTupleList v =
-  LazyTupleList (Unit -> Maybe (Tuple v (LazyTupleList v)))
-derive newtype instance lazyLazyTupleList :: Z.Lazy (LazyTupleList v)
-
-mapValues
-  :: forall r t r' t' fields
-   . RowToList r fields
-  => MapValues fields r r' t t'
-  => (t -> t')
-  -> Record r
-  -> Record r'
-mapValues f r = build (mapValuesImpl (RLProxy :: RLProxy fields) f r) {}
-
-class ( Homogeneous row fieldType
-      , HomogeneousRowList rl fieldType
-      , Homogeneous row' fieldType'
-      )
-   <= MapValues rl row row' fieldType fieldType'
-    | rl -> row'
-    , row' -> fieldType'
-    , row -> fieldType
-  where
-    mapValuesImpl
-      :: RLProxy rl
-      -> (fieldType -> fieldType')
-      -> Record row
-      -> Builder {} (Record row')
-
-instance mapValuesCons ::
-  ( MapValues tail row tailRow' fieldType fieldType'
-  , Homogeneous row fieldType
-  , Homogeneous row' fieldType'
-  , IsSymbol name
-  , RowCons name fieldType tailRow row
-  , Lacks name tailRow'
-  , RowCons name fieldType' tailRow' row'
-  ) => MapValues (Cons name fieldType tail) row row' fieldType fieldType'
-  where
-    mapValuesImpl _ f record = insert nameP value <<< rest
-      where
-        nameP = SProxy :: SProxy name
-        value = f (get nameP record)
-        rest = mapValuesImpl (RLProxy :: RLProxy tail) f record
-
-instance mapValuesNil
-  :: Homogeneous row fieldType
-  => MapValues Nil row () fieldType fieldType'
-  where
-    mapValuesImpl _ _ _ = id
-
-mapWithIndex
-  :: forall r t r' t' fields
-   . RowToList r fields
-  => MapWithIndex fields r r' t t'
-  => (String -> t -> t')
-  -> Record r
-  -> Record r'
-mapWithIndex f r = build (mapWithIndexImpl (RLProxy :: RLProxy fields) f r) {}
-
-class ( Homogeneous row fieldType
-      , HomogeneousRowList rl fieldType
-      , Homogeneous row' fieldType'
-      )
-   <= MapWithIndex rl row row' fieldType fieldType'
-    | rl -> row'
-    , row' -> fieldType'
-    , row -> fieldType
-  where
-    mapWithIndexImpl
-      :: RLProxy rl
-      -> (String -> fieldType -> fieldType')
-      -> Record row
-      -> Builder {} (Record row')
-
-instance mapWithIndexCons ::
-  ( MapWithIndex tail row tailRow' fieldType fieldType'
-  , Homogeneous row fieldType
-  , Homogeneous row' fieldType'
-  , IsSymbol name
-  , RowCons name fieldType tailRow row
-  , RowLacks name tailRow'
-  , RowCons name fieldType' tailRow' row'
-  ) => MapWithIndex (Cons name fieldType tail) row row' fieldType fieldType'
-  where
-    mapWithIndexImpl _ f record = insert nameP value <<< rest
-      where
-        nameP = SProxy :: SProxy name
-        rest = mapWithIndexImpl (RLProxy :: RLProxy tail) f record
-        value = f (reflectSymbol nameP) (get nameP record)
-
-instance mapWithIndexNil
-  :: Homogeneous row fieldType
-  => MapWithIndex Nil row () fieldType fieldType'
-  where
-    mapWithIndexImpl _ _ _ = id
+import Prim.Row as Row
+import Prim.RowList as RL
 
 foldlValues
-  :: forall b r t fields
-   . RowToList r fields
-  => FoldlValues fields r t
-  => (b -> t -> b)
-  -> b
-  -> Record r
-  -> b
-foldlValues = foldlValuesImpl (RLProxy :: RLProxy fields)
+  :: forall accum row fieldType rowList
+   . RL.RowToList row rowList
+  => FoldlValues rowList row fieldType
+  => (accum -> fieldType -> accum)
+  -> accum
+  -> Record row
+  -> accum
+foldlValues = foldlValuesImpl (RLProxy :: RLProxy rowList)
 
-class ( Homogeneous row fieldType
-      , HomogeneousRowList rl fieldType
-      )
-   <= FoldlValues rl row fieldType
-    | row -> fieldType
+class
+  ( Homogeneous row fieldType
+  , HomogeneousRowList rowList fieldType
+  )
+  <= FoldlValues (rowList :: RL.RowList) (row :: # Type) fieldType
+  | rowList -> row fieldType
   where
     foldlValuesImpl
-      :: forall b
-       . RLProxy rl
-      -> (b -> fieldType -> b)
-      -> b
+      :: forall accum
+       . RLProxy rowList
+      -> (accum -> fieldType -> accum)
+      -> accum
       -> Record row
-      -> b
+      -> accum
 
 instance foldlValuesCons ::
-  ( FoldlValues tail row fieldType
-  , Homogeneous row fieldType
+  ( FoldlValues tailRowList row fieldType
+  , Homogeneous tailRow fieldType
+  , HomogeneousRowList tailRowList fieldType
+  , HomogeneousRowList trash fieldType
   , IsSymbol name
-  , RowCons name fieldType tailRow row
-  ) => FoldlValues (Cons name fieldType tail) row fieldType
+  , RL.RowToList row trash
+  , Row.Cons name fieldType tailRow row
+  ) => FoldlValues (RL.Cons name fieldType tailRowList) row fieldType
   where
-    foldlValuesImpl _ f acc record = foldlValuesImpl tailProxy f acc' record
-        where
-          tailProxy = (RLProxy :: RLProxy tail)
-          value = get (SProxy :: SProxy name) record
-          acc' = f acc value
+    foldlValuesImpl _ f accum record = foldlValuesImpl tailProxy f accum' record
+      where
+        tailProxy :: RLProxy tailRowList
+        tailProxy = RLProxy
+
+        value :: fieldType
+        value = Record.get (SProxy :: SProxy name) record
+
+        accum' = f accum value
 
 instance foldlValuesNil
   :: Homogeneous row fieldType
-  => FoldlValues Nil row fieldType
+  => FoldlValues RL.Nil row fieldType
   where
-    foldlValuesImpl _ _ acc _ = acc
+    foldlValuesImpl _ _ accum _ = accum
 
--- | Not stack safe, but realistically this shouldn't matter on records.
-foldrValues
-  :: forall b r t fields
-   . RowToList r fields
-  => FoldrValues fields r t
-  => (t -> b -> b)
-  -> b
-  -> Record r
-  -> b
-foldrValues = foldrValuesImpl (RLProxy :: RLProxy fields)
+foldlValuesWithIndex
+  :: forall accum row fieldType rowList
+   . RL.RowToList row rowList
+  => FoldlValuesWithIndex rowList row fieldType
+  => (accum -> String -> fieldType -> accum)
+  -> accum
+  -> Record row
+  -> accum
+foldlValuesWithIndex = foldlValuesWithIndexImpl (RLProxy :: RLProxy rowList)
 
-class ( Homogeneous row fieldType
-      , HomogeneousRowList rl fieldType
-      )
-   <= FoldrValues rl row fieldType
-    | row -> fieldType
+class
+  ( Homogeneous row fieldType
+  , HomogeneousRowList rowList fieldType
+  )
+  <= FoldlValuesWithIndex (rowList :: RL.RowList) (row :: # Type) fieldType
+  | rowList -> row fieldType
   where
-    foldrValuesImpl
-      :: forall b
-       . RLProxy rl
-      -> (fieldType -> b -> b)
-      -> b
+    foldlValuesWithIndexImpl
+      :: forall accum
+       . RLProxy rowList
+      -> (accum -> String -> fieldType -> accum)
+      -> accum
       -> Record row
-      -> b
+      -> accum
 
-instance foldrValuesCons ::
-  ( FoldrValues tail row fieldType
-  , Homogeneous row fieldType
+instance foldlValuesWithIndexCons ::
+  ( FoldlValuesWithIndex tailRowList row fieldType
+  , Homogeneous tailRow fieldType
+  , HomogeneousRowList tailRowList fieldType
+  , HomogeneousRowList trash fieldType
   , IsSymbol name
-  , RowCons name fieldType tailRow row
-  ) => FoldrValues (Cons name fieldType tail) row fieldType
+  , RL.RowToList row trash
+  , Row.Cons name fieldType tailRow row
+  ) => FoldlValuesWithIndex (RL.Cons name fieldType tailRowList) row fieldType
   where
-    foldrValuesImpl _ f b rec = f value (foldrValuesImpl tailProxy f b rec)
-        where
-          tailProxy = (RLProxy :: RLProxy tail)
-          value = get (SProxy :: SProxy name) rec
-
-instance foldrValuesNil
-  :: Homogeneous row fieldType
-  => FoldrValues Nil row fieldType
-  where
-    foldrValuesImpl _ _ b _ = b
-
-foldrValuesLazy
-  :: forall b r t fields
-   . Z.Lazy b
-  => RowToList r fields
-  => FoldrValuesLazy fields r t
-  => (t -> b -> b)
-  -> b
-  -> Record r
-  -> b
-foldrValuesLazy = foldrValuesLazyImpl (RLProxy :: RLProxy fields)
-
-class ( Homogeneous row fieldType
-      , HomogeneousRowList rl fieldType
-      )
-   <= FoldrValuesLazy rl row fieldType
-    | row -> fieldType
-  where
-    foldrValuesLazyImpl
-      :: forall b
-       . Z.Lazy b
-      => RLProxy rl
-      -> (fieldType -> b -> b)
-      -> b
-      -> Record row
-      -> b
-
-instance foldrValuesLazyCons ::
-  ( FoldrValuesLazy tail row fieldType
-  , Homogeneous row fieldType
-  , IsSymbol name
-  , RowCons name fieldType tailRow row
-  ) => FoldrValuesLazy (Cons name fieldType tail) row fieldType
-  where
-    foldrValuesLazyImpl _ f b rec = Z.defer \_ ->
-      f value (foldrValuesLazyImpl tailProxy f b rec)
+    foldlValuesWithIndexImpl _ f accum record = foldlValuesWithIndexImpl tailProxy f accum' record
       where
-        tailProxy = (RLProxy :: RLProxy tail)
-        value = get (SProxy :: SProxy name) rec
+        tailProxy :: RLProxy tailRowList
+        tailProxy = RLProxy
 
-instance foldrValuesLazyNil
+        value :: fieldType
+        value = Record.get (SProxy :: SProxy name) record
+
+        key :: String
+        key = reflectSymbol (SProxy :: SProxy name)
+
+        accum' = f accum key value
+
+instance foldlValuesWithIndexNil
   :: Homogeneous row fieldType
-  => FoldrValuesLazy Nil row fieldType
+  => FoldlValuesWithIndex RL.Nil row fieldType
   where
-    foldrValuesLazyImpl _ _ b _ = b
+    foldlValuesWithIndexImpl _ _ accum _ = accum
--- test/Main.purs
+++ test/Main.purs
@@ -2,21 +2,16 @@ module Test.Main where
 
 import Prelude
 
-import Effect
-import Data.List as L
-import Record (equal)
-import Record.Homogeneous (valuesToUnfoldable, mapValues, mapWithIndex, foldlValues, foldrValues)
+import Effect (Effect)
+-- | import Data.List as L
+-- | import Record (equal)
+import Record.Homogeneous (foldlValues, foldlValuesWithIndex)
 import Test.Assert (assert')
 
 main :: Effect Unit
 main = do
-  assert' "mapValues" $
-    mapValues (_ + 1) {a: 1, b: 2} `equal` {a: 2, b: 3}
-  assert' "mapWithIndex" $
-    mapWithIndex (\l v -> l <> show v) {a: 1, b: 2} `equal` {a: "a1", b: "b2"}
   assert' "foldlValues" $
     foldlValues (+) 0 {a: 1, b: 2, c: 3} == 6
-  assert' "foldrValues" $
-    foldrValues (+) 0 {a: 1, b: 2, c: 3} == 6
-  assert' "valuesToUnfoldable" $
-    valuesToUnfoldable {a: 1, b: 2, c: 3} == L.fromFoldable [1, 2, 3]
+
+  assert' "foldlValuesWithIndex" $
+    foldlValuesWithIndex (\acc key val -> acc <> key <> show val) "" {a: 1, b: 2, c: 3} == "a1b2c3"
