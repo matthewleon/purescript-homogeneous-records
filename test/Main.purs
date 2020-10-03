3b4afe2286b7b47f83c5795759b8bffec7824029
Serhii Khoma <srghma@gmail.com>
2020-10-03 10:59:40 +0300

    feat: mapValuesWithIndex

--- test/Main.purs
+++ test/Main.purs
@@ -3,15 +3,16 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
--- | import Data.List as L
--- | import Record (equal)
-import Record.Homogeneous (foldlValues, foldlValuesWithIndex)
+import Record.Homogeneous
 import Test.Assert (assert')
 
 main :: Effect Unit
 main = do
   assert' "foldlValues" $
-    foldlValues (+) 0 {a: 1, b: 2, c: 3} == 6
+    foldlValues (+) 0 { a: 1, b: 2, c: 3 } == 6
 
   assert' "foldlValuesWithIndex" $
-    foldlValuesWithIndex (\acc key val -> acc <> key <> show val) "" {a: 1, b: 2, c: 3} == "a1b2c3"
+    foldlValuesWithIndex (\acc key val -> acc <> key <> show val) "" { a: 1, b: 2, c: 3 } == "a1b2c3"
+
+  assert' "mapValuesWithIndex" $
+    mapValuesWithIndex (\key val -> key <> show val) {a: 1, b: 2, c: 3} == { a: "a1", b: "b2", c: "c3" }
